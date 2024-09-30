8ee3e52861c4464ffce9606abb15bc9e65dbe8e1
mituc794 <153087940+mituc794@users.noreply.github.com>
2024-09-30 12:49:56 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+
+# Load image, grayscale, Otsu's threshold
+image = cv2.imread('demo.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Filter out large non-connecting objects
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area < 100:
+        cv2.drawContours(thresh,[c],0,0,-1)
+
+# Morph open using elliptical shaped kernel
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=3)
+
+# Find circles 
+cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    area = cv2.contourArea(c)
+    #if area > 20 and area < 50:
+    if area > 200:
+        ((x, y), r) = cv2.minEnclosingCircle(c)
+        cv2.circle(image, (int(x), int(y)), int(r), (36, 255, 12), 2)
+
+cv2.imshow('opening', opening)
+cv2.imshow('image', image)
+cv2.waitKey()
